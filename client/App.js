ad7ceab1ceaeeca5864624fdc4a63283a0f89e74
Dylan Widjaja <dwidjaja945@gmail.com>
2018-08-22 16:24:05 -0700

    added epub libraries in attempt to try to parse text from cfi range. Attempting to try different method without using epubjs-rn to obtain text

--- client/App.js
+++ client/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Alert } from "react-native";
+import { StyleSheet, Text, View, TouchableHighlight, Alert , WebView } from "react-native";
 
 import { Epub } from 'epubjs-rn';
 
 const epubCFI = require('./lib/EpubCFI/src/epubcfi');
 
+
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
-		// this._onPressButton = this._onLongPressButton.bind(this);
+
 		this.onPress = this.onPress.bind(this);
 	}
-	onLongPress = (event) => {		
-		// console.log("Arguments: ", arguments);
+	onLongPress = (event) => {
 		console.log("epubCFI.prototype.parse(): " , epubCFI.prototype.parse(event));
 		console.log("epubCFI.prototype.parseComponent(): ", epubCFI.prototype.parseComponent(event));
 		console.log("epubCFI.prototype.getRange(): ", epubCFI.prototype.getRange(event));
-		// Alert.alert(event.target)
 	}
 
 	onPress(event){
@@ -27,17 +26,25 @@ export default class App extends React.Component {
 		debugger;
 		console.log('event', event)
 		console.log('epubCFI.prototype.parse(event)', epubCFI.prototype.parse(event));
-		console.log("epubCFI.prototype.getRange(event)", epubCFI.prototype.getRange(event));;
+		const parsedEvent = epubCFI.prototype.parse(event);
+
+debugger;
+		// console.log("epubCFI.prototype.getRange(event)", epubCFI.prototype.getRange(event));
 	}
 
 	render() {
+
 		return (
 		// 	<TouchableHighlight style={styles.container}>
 		// 		<Text style={styles.button} onPress={ (event) => { this.onPress(event) } } >Testing this Highlight</Text>
 		// 	</TouchableHighlight>
 		// )
-			<Epub onSelected={ (event, a) => this.selectText(event, a) } style={styles.epub} onLongPress={this.onLongPress.bind(this)} src={"https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf"}
-			flow="scrolled" />
+		// <WebView
+		// 	source={{ uri: 'https://github.com/facebook/react-native' }}
+		// 	style={{ marginTop: 20 }}
+		// />
+		<Epub onSelected={ (event, a) => this.selectText(event, a) } style={styles.epub} onLongPress={this.onLongPress.bind(this)} src={"https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf"}
+		flow="scrolled" />
 		)
 	}
 }
