438ee7115dc26b4948299c46ab19043197519c1e
Dylan Widjaja <dwidjaja945@gmail.com>
2018-09-05 12:57:48 -0700

    can now successfully get text from highlighted region can now implement into application

--- client/App.js
+++ client/App.js
@@ -1,49 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Alert , WebView } from "react-native";
+import { Clipboard , StyleSheet, Text, View, TouchableHighlight, Alert , WebView } from "react-native";
 
 import { Epub } from 'epubjs-rn';
-
-const epubCFI = require('./lib/EpubCFI/src/epubcfi');
-
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.onPress = this.onPress.bind(this);
-	}
-	onLongPress = (event) => {
-		console.log("epubCFI.prototype.parse(): " , epubCFI.prototype.parse(event));
-		console.log("epubCFI.prototype.parseComponent(): ", epubCFI.prototype.parseComponent(event));
-		console.log("epubCFI.prototype.getRange(): ", epubCFI.prototype.getRange(event));
+		this.state = {
+			highlightedText: ""
+		}
+		this.getHighlightedText = this.getHighlightedText.bind(this);
 	}
 
-	onPress(event){
-		console.log(event);
+	async _getContent() {
+		const content = await Clipboard.getString();
+		return content;
 	}
 
-	selectText = (event, a) => {
-		debugger;
-		console.log('event', event)
-		console.log('epubCFI.prototype.parse(event)', epubCFI.prototype.parse(event));
-		const parsedEvent = epubCFI.prototype.parse(event);
+	getHighlightedText() {
+		return new Promise( (resolve , reject) => {
+			try {
+				const highlightedText = this._getContent();
+				resolve(highlightedText);
+			} catch ( ex ) {
+				reject(ex);
+				console.log(ex);
+			}
+		});
+	}
 
-debugger;
+	selectText = (event, rendition) => {
+		Promise.all([this.getHighlightedText()])
+			.then((data) => {
+				this.setState({
+					highlightedText: data[0]
+				});
+			})
+			.catch( ( err ) => {
+				console.log("promise errors" , err );
+			} );
+
+		// console.log('event', event);
+		// console.log('epubCFI.prototype.parse(event)', epubCFI.prototype.parse(event));
+		// const parsedEvent = epubCFI.prototype.parse(event);
+		// console.log("window" , window);
+
 		// console.log("epubCFI.prototype.getRange(event)", epubCFI.prototype.getRange(event));
 	}
 
 	render() {
 
 		return (
-		// 	<TouchableHighlight style={styles.container}>
-		// 		<Text style={styles.button} onPress={ (event) => { this.onPress(event) } } >Testing this Highlight</Text>
-		// 	</TouchableHighlight>
-		// )
-		// <WebView
-		// 	source={{ uri: 'https://github.com/facebook/react-native' }}
-		// 	style={{ marginTop: 20 }}
-		// />
-		<Epub onSelected={ (event, a) => this.selectText(event, a) } style={styles.epub} onLongPress={this.onLongPress.bind(this)} src={"https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf"}
+		<Epub onSelected={ (event, a) => this.selectText(event, a) } style={styles.epub} src={"https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf"}
 		flow="scrolled" />
 		)
 	}
